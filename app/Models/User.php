823b4a828236b41a732f38b1c9f31bfbb468b5de
Andres Teran <pipre94@gmail.com>
2023-05-31 17:59:19 -0500

    se refactoriza codigo

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class User
- *
- * @property $id
- * @property $username
- * @property $password
- * @property $name
- * @property $mail
- *
- * @package App
- * @mixin \Illuminate\Database\Eloquent\Builder
- */
 class User extends Model
 {
-    
-    static $rules = [
-		'username' => 'required',
-		'name' => 'required',
-		'mail' => 'required',
-    ];
-
-    protected $perPage = 20;
-
-    /**
-     * Attributes that should be mass-assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['username','name','mail'];
-
-
-
+    public $timestamps = false;
+    protected $fillable = ['username ', 'password', 'name', 'mail'];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PropertyController;
+use App\Models\Propertie;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +17,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//posts
-Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
-Route::apiResource('/users', App\Http\Controllers\Api\UserController::class);
-Route::apiResource('/properties', App\Http\Controllers\Api\PropertyController::class);
+// crud para usuarios
+Route::get('user',function(){
+    return User::all();
+});
+
+Route::get('user/{id}',function($id){
+    return User::find($id);
+});
+
+Route::post('user',function (Request $request){
+    $valid = User::create($request-> all());
+    return response()->json($valid);
+});
+
+Route::put('user/{id}', function(Request $request, $id){
+    $propertie = User::findOrFail($id);
+    $propertie->update($request->all());
+    return $propertie;
+});
+
+Route::delete('user/{id}', function($id){
+
+    $encontrar = User::find($id);
+    if($encontrar != null){
+        Propertie::find($id)->delete();
+        return response()->json([
+            'code' => '200',
+            'message' => "Successfully created",
+            'success' => true
+        ], 200);
+    }
+
+    return response()->json([
+        'code' => '100',
+        'message' => "Propiedad no eliminada",
+        'success' => true
+    ], 200);
+});
+
+
+
+// Crud para propiedades
+Route::get('properties',function(){
+    return Propertie::all();
+});
+
+Route::get('properties/{id}',function($id){
+    return Propertie::find($id);
+});
+
+Route::post('properties',function (Request $request){
+    $valid = Propertie::create($request-> all());
+    return response()->json($valid);
+});
+
+Route::put('properties/{id}', function(Request $request, $id){
+    $propertie = Propertie::findOrFail($id);
+    $propertie->update($request->all());
+    return $propertie;
+});
+
+Route::delete('properties/{id}', function($id){
+
+    $encontrar = Propertie::find($id);
+    if($encontrar != null){
+        Propertie::find($id)->delete();
+        return response()->json([
+            'code' => '200',
+            'message' => "Successfully created",
+            'success' => true
+        ], 200);
+    }
+
+    return response()->json([
+        'code' => '100',
+        'message' => "Propiedad no eliminada",
+        'success' => true
+    ], 200);
+});
+
